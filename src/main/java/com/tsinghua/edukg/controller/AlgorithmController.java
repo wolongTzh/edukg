f3589d0dcf245022d6b491f2fcbe5ce701d64b9b
tanzheng <tanzheng@tsinghua.edu.cn>
2023-03-09 09:50:05 +0800

    增加irqa返回结果

--- src/main/java/com/tsinghua/edukg/controller/AlgorithmController.java
+++ src/main/java/com/tsinghua/edukg/controller/AlgorithmController.java
@@ -10,6 +10,7 @@ import com.tsinghua.edukg.api.model.EntityLinkParam;
 import com.tsinghua.edukg.api.model.QAParam;
 import com.tsinghua.edukg.api.model.QAResult;
 import com.tsinghua.edukg.controller.utils.AlgorithmControllerUtil;
+import com.tsinghua.edukg.model.VO.CombineQaVO;
 import com.tsinghua.edukg.model.WebResInfo;
 import com.tsinghua.edukg.service.CombineService;
 import com.tsinghua.edukg.utils.CommonUtil;
@@ -21,7 +22,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 /**
  * api controller
@@ -55,11 +58,10 @@ public class AlgorithmController {
      * @return
      */
     @PostMapping(value = "qa")
-    public WebResInfo inputQuestion(@RequestBody QAParam param) throws IllegalAccessException {
+    public WebResInfo inputQuestion(@RequestBody QAParam param) throws IllegalAccessException, InterruptedException, ExecutionException, IOException {
         AlgorithmControllerUtil.validInputQuestionParam(param);
-        ApiResult<QAResult> qaResult = qaFeignService.qaRequest(CommonUtil.entityToMutiMap(param));
-        QAResult accQaResult = qaResult.getAnswerData();
-        return WebUtil.successResult(accQaResult);
+        CombineQaVO qaResult = combineService.simpleQA(param);
+        return WebUtil.successResult(qaResult);
     }
 
     /**
