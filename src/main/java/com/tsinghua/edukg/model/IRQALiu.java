23b2889404878d82a9e187c6e3bab4657080361a
tanzheng <tanzheng@tsinghua.edu.cn>
2023-04-14 15:36:45 +0800

    实体上传

---
+++ src/main/java/com/tsinghua/edukg/model/IRQALiu.java
@@ -0,0 +1,21 @@
+package com.tsinghua.edukg.model;
+
+import lombok.Data;
+
+/**
+ * 刘阳版本irqa结果
+ *
+ * @author tanzheng
+ * @date 2022/11/8
+ */
+@Data
+public class IRQALiu {
+
+    String subject;
+
+    String predicate;
+
+    String value;
+
+    String all;
+}
