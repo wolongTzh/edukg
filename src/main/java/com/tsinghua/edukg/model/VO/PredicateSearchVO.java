2373b257860b62e00f6beab25b52ccee6000ae22
tanzheng <tanzheng@tsinghua.edu.cn>
2023-05-29 15:47:05 +0800

    新增的类

---
+++ src/main/java/com/tsinghua/edukg/model/VO/PredicateSearchVO.java
@@ -0,0 +1,13 @@
+package com.tsinghua.edukg.model.VO;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PredicateSearchVO {
+
+    String predicateDes;
+
+    String example;
+}
